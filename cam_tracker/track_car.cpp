1fca71da23d7520437291ae9a0667476c60bb3bf
samuel.j.bishop@gmail.com <samuel.j.bishop@gmail.com@9657ef81-e397-aa81-a370-0c0a57d1ce47>
2014-09-16 04:14:16 +0000

    Restrict the search for keypoints to an area where the car is expected to be found.

--- cam_tracker/track_car.cpp
+++ cam_tracker/track_car.cpp
@@ -4,8 +4,11 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <vector>
+
+#include "util.h"
 
 using namespace cv;
 using namespace std;
@@ -46,38 +49,67 @@ int main(int argc, char **argv)
     Mat training_image = imread("car.png", 0);
     vector<KeyPoint> training_image_keypoints;
     sift.detect(training_image, training_image_keypoints, Mat());
-    printf("# kps: %lu\n", training_image_keypoints.size());
+    printf("training image # kps: %lu\n", training_image_keypoints.size());
     Mat training_image_descriptors;
     sift.compute(training_image, training_image_keypoints, training_image_descriptors);
     Point2f training_center =
         find_center(training_image, training_image_keypoints);
 
-    Mat query_image = imread("car-snapshot.png", 0);
-    vector<KeyPoint> query_image_keypoints;
-    sift.detect(query_image, query_image_keypoints, Mat());
-    printf("# kps: %lu\n", query_image_keypoints.size());
-    Mat query_image_descriptors;
-    sift.compute(query_image, query_image_keypoints, query_image_descriptors);
+    VideoCapture capture = open_stream(argv[2]);
+    Mat bgr_frame;
+    int key = -1;
+    static const char *OUTPUT_WINDOW = "Output Window";
+    namedWindow(OUTPUT_WINDOW, WINDOW_AUTOSIZE);
+    const Scalar RED(0, 0, 255);
+    while (key == -1 && capture.read(bgr_frame)) {
+        Mat query_image;
+        cvtColor(bgr_frame, query_image, CV_BGR2GRAY);
+        vector<KeyPoint> query_image_keypoints;
 
-    vector<vector<DMatch> > matches;
-    matcher.knnMatch(training_image_descriptors,
-            query_image_descriptors,
-            matches,
-            2);
-    vector<DMatch> good_matches;
-    printf("%lu\n", matches.size());
-    for (unsigned i = 0; i < matches.size(); ++i) {
-        if (matches[i][0].distance < .75 * matches[i][1].distance) {
-            good_matches.push_back(matches[i][0]);
+        Mat mask = Mat::zeros(query_image.size(), CV_8U);
+        Mat roi(mask, cv::Rect(390, 250, 100, 60));
+        roi = Scalar(255, 255, 255);
+        rectangle(bgr_frame, Point(390, 250), Point(490, 310), RED);
+
+        sift.detect(query_image, query_image_keypoints, mask);
+        printf("query image # kps: %3lu", query_image_keypoints.size());
+        if (query_image_keypoints.size() == 0) {
+            printf("\n");
+            imshow(OUTPUT_WINDOW, bgr_frame);
+            continue;
         }
-    }
-    printf("%lu\n", good_matches.size());
+        Mat query_image_descriptors;
+        sift.compute(query_image, query_image_keypoints, query_image_descriptors);
 
-    Mat M = find_homography(good_matches, training_image_keypoints, query_image_keypoints);
-    vector<Point2f> training_points;
-    training_points.push_back(training_center);
-    vector<Point2f> query_points;
-    perspectiveTransform(training_points, query_points, M);
-    printf("%f, %f\n", query_points[0].x, query_points[0].y);
+        vector<vector<DMatch> > matches;
+        matcher.knnMatch(training_image_descriptors,
+                query_image_descriptors,
+                matches,
+                2);
+        vector<DMatch> good_matches;
+        printf(", # of matches: %3lu", matches.size());
+        for (unsigned i = 0; i < matches.size(); ++i) {
+            if (matches[i][0].distance < .75 * matches[i][1].distance) {
+                good_matches.push_back(matches[i][0]);
+            }
+        }
+        printf(", # of good matches: %3lu", good_matches.size());
+        if (good_matches.size() < 10) { // 4 is minimum
+            printf("\n");
+            imshow(OUTPUT_WINDOW, bgr_frame);
+            continue;
+        }
+
+        Mat M = find_homography(good_matches, training_image_keypoints, query_image_keypoints);
+        vector<Point2f> training_points;
+        training_points.push_back(training_center);
+        vector<Point2f> query_points;
+        perspectiveTransform(training_points, query_points, M);
+        printf(", car center: %f, %f\n", query_points[0].x, query_points[0].y);
+
+        circle(bgr_frame, Point2i(int(query_points[0].x), int(query_points[0].y)), 2, RED, -1);
+        imshow(OUTPUT_WINDOW, bgr_frame);
+        key = waitKey(1);
+    }
 }
 
