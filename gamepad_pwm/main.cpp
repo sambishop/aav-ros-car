742e495385e3fbb8604074e27baa4ca6784acc48
samuel.j.bishop@gmail.com <samuel.j.bishop@gmail.com@9657ef81-e397-aa81-a370-0c0a57d1ce47>
2014-11-01 05:49:07 +0000

    I think this is working.  I'm handling the drive and steer PWM device nodes separately now.  The mapping math appears to work correctly.  I have also switched the sign of the Y axis so that the reverse direction results in a negative number.

--- gamepad_pwm/main.cpp
+++ gamepad_pwm/main.cpp
@@ -1,5 +1,7 @@
 #include <errno.h>
 #include <fcntl.h>
+#include <inttypes.h>
+#include <limits.h>
 #include <linux/joystick.h>
 #include <poll.h>
 #include <stdio.h>
@@ -7,15 +9,22 @@
 #include <string.h>
 #include <unistd.h>
 
+static double STEER_MIN_MS = 1.0;
+static double STEER_NEUTRAL_MS = 1.5;
+static double STEER_MAX_MS = 2.0;
+static double DRIVE_MIN_MS = 1.1;
+static double DRIVE_NEUTRAL_MS = 1.5;
+static double DRIVE_MAX_MS = 1.9;
+
 int openGamepadDevice(const char *gamepadPath)
 {
-    int jsFd = open(gamepadPath, O_RDONLY | O_NONBLOCK);
-    if (jsFd == -1) {
+    int gamepadFd = open(gamepadPath, O_RDONLY | O_NONBLOCK);
+    if (gamepadFd == -1) {
         fprintf(stderr, "\"%s\" attempting to open \"%s\" for reading\n",
                 strerror(errno), gamepadPath);
         exit(1);
     }
-    return jsFd;
+    return gamepadFd;
 }
 
 bool isAxisEvent(js_event event)
@@ -23,9 +32,13 @@ bool isAxisEvent(js_event event)
     return ((event.type & ~JS_EVENT_INIT) == JS_EVENT_AXIS) && !(~1U & event.number);
 }
 
-void processEvent(js_event event, int *hortizontalAxis, int *verticalAxis)
+void processEvent(js_event event, int *horizontalAxis, int *verticalAxis)
 {
-    *((event.number & 1) ? verticalAxis : hortizontalAxis) = event.value;
+    if (event.number & 1) {
+        *verticalAxis = -event.value;
+    } else {
+        *horizontalAxis = event.value;
+    }
 }
 
 FILE *openPwmDevice(const char *pwmPath)
@@ -35,31 +48,60 @@ FILE *openPwmDevice(const char *pwmPath)
         fprintf(stderr, "\"%s\" attempting to open \"%s\" for reading", strerror(errno), pwmPath);
         exit(1);
     }
+    setvbuf(file, NULL, _IOLBF, BUFSIZ);
     return file;
 }
 
-void eventLoop(int jsFd, const char *gamepadPath)
+void writePwmDuty(FILE *pwmFile, unsigned long long nanoseconds)
+{
+    fprintf(pwmFile, "%llu\n", nanoseconds);
+}
+
+unsigned long long calculateNanos(double min, double max, double neutral, int value)
+{
+    static const int NS_PER_MS = 1000000;
+    if (value < 0) {
+        fprintf(stderr, "< 0 %10llu\n", (unsigned long long) (((neutral - min) / 32767 * value + min) * NS_PER_MS));
+        return (unsigned long long) (((neutral - min) / 32767 * value + min) * NS_PER_MS);
+    } else if (value > 0) {
+        fprintf(stderr, "> 0 %10llu\n", (unsigned long long) (((max - neutral) / 32767 * value + neutral) * NS_PER_MS));
+        return (unsigned long long) (((max - neutral) / 32767 * value + neutral) * NS_PER_MS);
+    } else {
+        fprintf(stderr, "  0 %10llu\n", (unsigned long long) (neutral * NS_PER_MS));
+        return (unsigned long long) (neutral * NS_PER_MS);
+    }
+}
+
+void eventLoop(int gamepadFd, FILE *steerFile, FILE *driveFile)
 {
     pollfd p;
-    p.fd = jsFd;
+    p.fd = gamepadFd;
     p.events = POLLIN;
-    int hortizontalAxis = 0;
-    int verticalAxis = 0;
+    int horizontalAxis = INT_MAX;
+    int verticalAxis = INT_MAX;
 
     while (1) {
         js_event event;
-        while (read(jsFd, &event, sizeof(event)) > 0) {
+        int previousHorizontal = horizontalAxis;
+        int previousVertical = verticalAxis;
+        while (read(gamepadFd, &event, sizeof(event)) > 0) {
             if (isAxisEvent(event)) {
-                processEvent(event, &hortizontalAxis, &verticalAxis);
-                fprintf(stderr, "[%d, %d]\n", hortizontalAxis, verticalAxis);
+                processEvent(event, &horizontalAxis, &verticalAxis);
             }
         }
 
         if (errno != EAGAIN) {
-            fprintf(stderr, "\"%s\" attempting to read from \"%s\"\n",
-                    strerror(errno), gamepadPath);
+            fprintf(stderr, "\"%s\" reading the gamepad device\n", strerror(errno));
             exit(1);
         }
+
+        if (horizontalAxis != previousHorizontal) {
+            writePwmDuty(steerFile, calculateNanos(STEER_MIN_MS, STEER_MAX_MS, STEER_NEUTRAL_MS, horizontalAxis));
+        }
+        if (verticalAxis != previousVertical) {
+            writePwmDuty(driveFile, calculateNanos(DRIVE_MIN_MS, DRIVE_MAX_MS, DRIVE_NEUTRAL_MS, verticalAxis));
+        }
+        fprintf(stderr, "[%d, %d]\n", horizontalAxis, verticalAxis);
 
         poll(&p, 1, -1);
     }
@@ -67,16 +109,18 @@ void eventLoop(int jsFd, const char *gamepadPath)
 
 int main(int argc, char **argv)
 {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: gamepad_pwm GAMEPAD_NODE PWM_NODE\n");
+    if (argc != 4) {
+        fprintf(stderr, "Usage: gamepad_pwm GAMEPAD_NODE STEER_NODE DRIVE_NODE\n");
         exit(1);
     }
     const char *gamepadPath = argv[1];
-    const char *pwmPath = argv[2];
+    const char *steerPath = argv[2];
+    const char *drivePath = argv[3];
 
-    int jsFd = openGamepadDevice(gamepadPath);
-    FILE *pwmFile = openPwmDevice(pwmPath);
-    eventLoop(jsFd, gamepadPath);
+    int gamepadFd = openGamepadDevice(gamepadPath);
+    FILE *steerFile = openPwmDevice(steerPath);
+    FILE *driveFile = openPwmDevice(drivePath);
+    eventLoop(gamepadFd, steerFile, driveFile);
 
     return 0;
 }
