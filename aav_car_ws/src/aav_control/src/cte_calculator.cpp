882b5b592a17f25ae42b2a0d1fae3b84f9965414
Sam Bishop <samuel.j.bishop@gmail.com>
2015-04-08 22:08:25 -0600

    Strawman implementation that always returns NaN.

--- aav_car_ws/src/aav_control/src/cte_calculator.cpp
+++ aav_car_ws/src/aav_control/src/cte_calculator.cpp
@@ -1,11 +1,17 @@
 #include "cte_calculator.h"
 
+#include <limits>
+
 namespace aav_control
 {
 
 CteCalculator::CteCalculator(const aav_msgs::QuinticPath &path)
   : path_calculator_(path), previousPoint_(0, 0)
+{}
+
+double CteCalculator::calculate(const tf2::Vector3 &position)
 {
+  return std::numeric_limits<double>::quiet_NaN();
 }
 
 } // end namespace aav_control
