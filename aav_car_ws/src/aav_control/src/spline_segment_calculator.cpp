6c2057e5ac2e110f6c81203650be62763a588390
Sam Bishop <samuel.j.bishop@gmail.com>
2015-12-01 21:56:45 -0700

    Try to make it more clear what's being calculated.

--- aav_car_ws/src/aav_control/src/spline_segment_calculator.cpp
+++ aav_car_ws/src/aav_control/src/spline_segment_calculator.cpp
@@ -9,6 +9,9 @@ SplineSegmentCalculator::SplineSegmentCalculator(const aav_msgs::QuinticSplineSe
 
 double SplineSegmentCalculator::calculate(double t_1) const
 {
+  // The code below is an implementation of this function:
+  // S(t) = (1-t)^5*P0 + 5*(1-t)^4*t*P1 + 10*(1-t)^3*t^2*P2
+  //        + 10*(1-t)^2*t^3*P3 + 5(1-t)*t^4*P4 + t^5*P5
   double t_2 = t_1 * t_1;
   double t_3 = t_2 * t_1;
   double t_4 = t_2 * t_2;
@@ -30,6 +33,13 @@ double SplineSegmentCalculator::calculate(double t_1) const
 
 double SplineSegmentCalculator::calculate1stDerivative(double t_1) const
 {
+  // The code below is an implementation of this function:
+  // S'(t) = - 5*P0*    (1-t)^4 +  5*P1*    (1-t)^4
+  //        - 20*P1*t*  (1-t)^3
+  //        - 30*P2*t^2*(1-t)^2 + 20*P2*t*  (1-t)^3
+  //        - 20*P3*t^3*(1-t)   + 30*P3*t^2*(1-t)^2
+  //         - 5*P4*t^4         + 20*P4*t^3*(1-t)
+  //                             + 5*P5*t^4
   double t_2 = t_1 * t_1;
   double t_3 = t_2 * t_1;
   double t_4 = t_2 * t_2;
