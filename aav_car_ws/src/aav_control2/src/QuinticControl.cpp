918361a78af271c639fe5d1be20240ddb9b7af9c
Sam Bishop <samuel.j.bishop@gmail.com>
2015-12-03 22:08:15 -0700

    Delete this.  I'm keeping the aav_control package.

--- aav_car_ws/src/aav_control2/src/QuinticControl.cpp
+++
@@ -1,8 +0,0 @@
-#include "QuinticControl.h"
-
-using namespace aav_control;
-
-void QuinticControl::updateOdometry(const nav_msgs::Odometry::ConstPtr &odometry) {
-  fprintf(stderr, "updateOdometry() called\n");
-}
-
