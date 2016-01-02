3fec37082c48bc41c1f1c83f07d4a4a8192d3839
Sam Bishop <samuel.j.bishop@gmail.com>
2016-01-02 10:45:45 -0700

    ROSify curly brace placement

--- desktop_ws/src/aav_control/src/main.cpp
+++ desktop_ws/src/aav_control/src/main.cpp
@@ -5,7 +5,8 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, ROS_PACKAGE_NAME);
   ros::NodeHandle node;
 
